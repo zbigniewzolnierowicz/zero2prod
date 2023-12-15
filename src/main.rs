6179fdd804f5c2b4ed2af8e31f916a8ddaf25d5f
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@allegro.com>
2023-12-15 12:32:14 +0100

    add healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn ping() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/healthz", web::get().to(ping)))
+        .bind(("127.0.0.1", 3000))?
+        .run()
+        .await
 }
