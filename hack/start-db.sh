9fae98e37a44abb8f2de18af5e9fde84b018c0fa
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@allegro.com>
2023-12-15 18:02:27 +0100

    add database

---
+++ hack/start-db.sh
@@ -0,0 +1,37 @@
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "\tcargo install sqlx-cli --no-default-features --features rustls,postgres"
+    echo >&2 "to install it."
+
+    exit 1
+fi
+
+ENV_FILE="$(dirname $0)/../.env"
+
+if ! [ -f "$ENV_FILE" ]; then
+    echo >&2 "Error: no .env file found."
+    echo >&2 "Please, copy the .env.example file to .env"
+    echo >&2 "\tcp .env.example .env"
+
+    exit 1
+fi
+
+source $(dirname $0)/../.env
+
+podman compose up -d
+
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+until psql -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -p "${POSTGRES_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
