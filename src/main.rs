0d5ce71e5ba04b5692b31f77c0b7ca8c9a75b8f8
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-12-16 22:08:51 +0100

    add logs for subscription endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::net::TcpListener;
+use env_logger::Env;
 
 use sqlx::PgPool;
-use zero2prod::run;
+use zero2prod::{run, configuration::Settings};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let config = zero2prod::configuration::Settings::get().expect("Failed to read configuration.");
+    let config = Settings::get().expect("Failed to read configuration.");
     let address = ("127.0.0.1", config.port);
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     run(TcpListener::bind(address).expect("Could not bind to address"), connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,18 +10,23 @@ pub struct SubscribeFormBody {
 }
 
 pub async fn subscribe(body: web::Form<SubscribeFormBody>, db: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!("REQUEST {request_id} | Adding a new subscriber: {} <{}>", body.name, body.email);
     match sqlx::query!(
-        "INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)",
+        "INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4) RETURNING id",
         Uuid::new_v4(),
         body.email,
         body.name,
         Utc::now()
     )
-    .execute(db.get_ref())
+    .fetch_one(db.get_ref())
     .await {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(subscriber) => { 
+            log::info!("REQUEST {request_id} | New subscriber saved. ID: {}", subscriber.id);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Could not add subscription: {}", e);
+            log::error!("REQUEST {request_id} | Failed to add new subscriber: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
